7a7be1259d9f3f267c86fe35839aa6a57e82f6b4
evakhromeeva <evakhromeeva@ozon.ru>
2021-02-12 17:30:00 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,76 @@
+<script>
+	import Logo from './assets/images/equus-logo.svg';
+	import PageText from './assets/posts/front-page.md';
+	export let name;
+</script>
+
+<main>
+	<div class="header">
+		<Logo width="80" height="80" />
+	</div>
+	<div class="main-body">
+		<div class="side-block">
+			<Logo width="300" height="300" />
+		</div>
+		<div class="main-info">
+			<h1>Hello {name}!</h1>
+			<PageText />
+		</div>
+		<div class="side-block">
+			<Logo width="300" height="300" />
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		max-width: 240px;
+	}
+
+	h1 {
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	@media (max-width: 640px) {
+		.side-block {
+			display: none;
+		}
+	}
+
+	.main-body {
+		display: flex;
+		flex-direction: row;
+	}
+
+	.main-info {
+		max-width: 1200px;
+		text-align: left;
+		margin: auto;
+		padding: 24px;
+	}
+
+	.side-block {
+		min-width: 300px;
+		max-width: 500px;
+		background-color: blueviolet;
+	}
+
+	.header {
+		height: 80px;
+		max-height: 80px;
+		width: 100%;
+		padding-left: 8px;
+		text-align: left;
+		background-color: blueviolet;
+		display: grid;
+	}
+</style>
---
+++ src/components/ui-components/UIButton.svelte
@@ -0,0 +1,21 @@
+<button
+    onclick="onclick"
+>
+    {title}
+</button>
+
+<script>
+    export let onclick;
+    export let title;
+</script>
+
+<style>
+    button {
+        padding: 8px;
+        color: wheat;
+        text-align: center;
+        background-color: blueviolet;
+        border-radius: 4px;
+        border: none;
+    }
+</style>
